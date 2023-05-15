d72706db6039c1b9166d6e9b79460d95b2c0f752
sven-peeters-stitchd-be <sven.peeters@stitchd.be>
2023-05-15 09:56:58 +0200

    webshop completed without tracking

---
+++ scripts/generic.js
@@ -0,0 +1,42 @@
+function refreshAccountInfoLabel() {
+    
+    var accountNavigation = document.getElementById("nav-account");
+
+    if (!accountNavigation)
+        return;
+
+    if (localStorage.userLoggedIn == "true") {
+        if (typeof logoutTabClicked === "function")
+            logoutTabClicked();
+        accountNavigation.text = "Account (" + localStorage.userName + ")";
+    }
+    else {
+        if (typeof loginTabClicked === "function")
+            loginTabClicked();
+        accountNavigation.text = "Account";
+    }
+   
+}
+
+function refreshCartCountLabel() {    
+   
+    var cartItemCountLabel = document.getElementById("cart-item-count");
+
+    if (!cartItemCountLabel)
+        return;
+
+    var itemCount = 0;
+
+    var cartContent = {};
+    cartContent.products = [];    
+
+    if (localStorage.cartContent != null) {
+        cartContent = JSON.parse(localStorage.cartContent);
+    }
+
+    for (var cartProductIndex in cartContent.products) { 
+        itemCount = itemCount + cartContent.products[cartProductIndex].quantity;
+    }   
+
+    cartItemCountLabel.innerText = itemCount;
+}
